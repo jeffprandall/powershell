a64bd0740631b7689fe7823eab50bb1e1a4be9b6
Jeff Randall <jeffprandall@hotmail.com>
2019-03-15 07:44:27 -0700

    Add files via upload

---
+++ microsoft/Set-UserRegistryForOneDriveKFM.ps1
@@ -0,0 +1,20 @@
+﻿$currentUser = $env:UserName
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+
+Try {
+  Set-ItemProperty -Path $registryPath -Name Desktop -Value "C:\Users\$currentUser\Desktop"
+  Write-Host 'Successfully changed the Desktop settings' -ForegroundColor Green
+} catch {
+  $err = $ErrorMessage = $_.Exception.Message
+  Write-Host $err -ForegroundColor Red
+}
+
+Try {
+  Set-ItemProperty -Path $registryPath -Name Personal -Value "C:\Users\$currentUser\Documents"
+  Write-Host 'Successfully changed the My Documents settings' -ForegroundColor Green
+} catch {
+  $err = $ErrorMessage = $_.Exception.Message
+  Write-Host $err -ForegroundColor Red
+}
+
+Read-Host -Prompt "Done - press Enter to exit"
